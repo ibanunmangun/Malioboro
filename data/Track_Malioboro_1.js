62465c68113e076d3358a39ac6189ac71d05a234
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2020-04-12 23:45:38 +0700

    Add files via upload

---
+++ data/Track_Malioboro_1.js
@@ -0,0 +1,16 @@
+var json_Track_Malioboro_1 = {
+"type": "FeatureCollection",
+"name": "Track_Malioboro_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Id": "0", "Length": 1439.63033333 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 110.366210879315972, -7.789833820100402 ], [ 110.365423667998783, -7.796301431858998 ], [ 110.364876538006314, -7.801082201392271 ], [ 110.364732895079726, -7.802151182963897 ] ] ] } },
+{ "type": "Feature", "properties": { "Id": "0", "Length": 774.26093194199996 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 110.366528637262292, -7.78726588426218 ], [ 110.367070943187969, -7.782883281598822 ] ] ] } },
+{ "type": "Feature", "properties": { "Id": "0", "Length": 0.0 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 110.364082278564936, -7.78926549851747 ], [ 110.366256624264707, -7.789464135897277 ] ] ] } },
+{ "type": "Feature", "properties": { "Id": "0", "Length": 0.0 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 110.366528637262405, -7.78726588426218 ], [ 110.366339098184852, -7.788805132299442 ], [ 110.366320062496769, -7.788847962597512 ], [ 110.366262955432632, -7.788862239363593 ], [ 110.36570140263531, -7.78881940906546 ] ] ] } },
+{ "type": "Feature", "properties": { "Id": "0", "Length": 0.0 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 110.366339098184852, -7.788805132299442 ], [ 110.366256624264707, -7.789464135897277 ], [ 110.366210879316057, -7.789833820100331 ] ] ] } },
+{ "type": "Feature", "properties": { "Id": "0", "Length": 0.0 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 110.365978620167283, -7.791742026916222 ], [ 110.365680637492247, -7.791730732246724 ] ] ] } },
+{ "type": "Feature", "properties": { "Id": "0", "Length": 0.0 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 110.365704318620288, -7.793995647859385 ], [ 110.365446646366451, -7.793975177980708 ] ] ] } },
+{ "type": "Feature", "properties": { "Id": "0", "Length": 0.0 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 110.365418253640058, -7.796348742008436 ], [ 110.365140888518923, -7.796320038734117 ] ] ] } },
+{ "type": "Feature", "properties": { "Id": "0", "Length": 0.0 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 110.365295635050785, -7.797420171510632 ], [ 110.365449459283269, -7.7974271241086 ] ] ] } }
+]
+}
